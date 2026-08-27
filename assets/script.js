45b93ce0d997f4eb105dc3c0ea8d03b39ab1e563
Vitoria F. <206957068+Vitoria-F-0@users.noreply.github.com>
2026-08-26 23:01:14 -0300

    Nova funcionalidade em desenvolvimento

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,21 @@
-const maoPlayer = document.querySelector('.player2mao')
+const maoPlayer2 = document.querySelector('.player2mao')
+const maoPlayer1 = document.querySelector('.player1mao')
 
 const baralho = [
     {nome: "Geralt of Rivia", forca: 15, posicao: "neutra"},
+    {nome: "Yennefer of Vengerberg", forca: 7, posicao: "neutra"},
+    {nome: "Cirilla Fiona Elen Riannon", forca: 15, posicao: "neutra"},
+    {nome: "Triss Merigold", forca: 7, posicao: "neutra"},
+    {nome: "Eredin", forca: 15, posicao: "neutra"},
+    {nome: "Dandelion", forca: 2, posicao: "neutra"},
+    {nome: "Vesemir", forca: 6, posicao: "neutra"},
+    {nome: "Sigismund Dijkstra", forca: 4, posicao: "neutra"},
+    {nome: "Crach an Craite", forca: 10, posicao: "neutra"},
+    {nome: "Zoltan Chivay", forca: 5, posicao: "neutra"}
 ];
 
-function exibirCartas() {
+function criarCartas() {
+    const baralhoPronto = []
     baralho.forEach(element => {
         const carta = document.createElement('div')
         carta.classList.add('card')
@@ -12,8 +23,23 @@ function exibirCartas() {
         `<div>Nome: ${element.nome}</div> 
         <div>Poder: ${element.forca}</div> 
         <div>Posição: ${element.posicao}</div>`
-        maoPlayer.appendChild(carta)
-    });
+        baralhoPronto.push(carta)
+    })
+    return baralhoPronto
 }
 
-exibirCartas()
+function randomizarBaralho(baralhoPronto) {
+    baralhoPronto.Math.floor(Math.random() *10)
+    return
+}
+
+/*function exibirCartas(randomizarBaralho) {  
+    for (let i = 0; i <5; i++) {
+        const cartaSorteada = cartasEmbaralhadas[i]
+    }
+    
+    push.maoPlayer2.appendChild(randomizarBaralho)
+    push.maoPlayer1.appendChild(randomizarBaralho)
+}
+
+exibirCartas() */
